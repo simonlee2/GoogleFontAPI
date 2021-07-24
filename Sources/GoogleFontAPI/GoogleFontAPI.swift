00bb5f26f3653d83e1ce4642db8c283e1724a5c9
Simon Lee <simon.lee@cardinalblue.com>
2021-07-24 15:13:35 +0800

    Update API key variable name

--- Sources/GoogleFontAPI/GoogleFontAPI.swift
+++ Sources/GoogleFontAPI/GoogleFontAPI.swift
@@ -2,7 +2,7 @@ import Alamofire
 
 public struct GoogleFontAPI {
     public func fetchAllFonts(completion: @escaping (Result<[GoogleFont], Error>) -> Void) {
-        let parameters = ["key": APIKey]
+        let parameters = ["key": GoogleFontAPIKey]
         AF.request("https://www.googleapis.com/webfonts/v1/webfonts", parameters: parameters)
             .validate()
             .responseDecodable(of: GoogleFontResponse.self) { response in
